8f078d89389401ee11c35f526a129a5aa478e503
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-06-21 20:45:37 +0200

    Clean Code Added template-form Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormGroupComponent } from './form-group/form-group.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormGroupComponent
+    FormGroupComponent,
+    TemplateFormComponent
   ],
   imports: [
     BrowserModule,
