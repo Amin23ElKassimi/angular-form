61ac7a9a04ad82f7bc1f3d5035a9ccd738a29a23
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-06-21 20:38:41 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroupComponent } from './form-group/form-group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormGroupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
