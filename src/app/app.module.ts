9fefd9aaceaaf586206b85c8905b409610b2a7a2
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-06-25 10:47:45 +0200

    clean code Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatInputModule} from '@angular/material/input'; 
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatSelectModule } from '@angular/material/select'; 
+import {MatRadioModule} from '@angular/material/radio'; 
+import {MatCheckboxModule} from '@angular/material/checkbox'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroupComponent } from './form-group/form-group.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormGroupComponent,
     TemplateFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-template-form',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
-  constructor() { }
+  options: FormGroup;
 
-  ngOnInit(): void {
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      hideRequired: false,
+      floatLabel: 'auto',
+    });
   }
 
+ ngOnInit(): void {
+  }
+
+ 
 }
