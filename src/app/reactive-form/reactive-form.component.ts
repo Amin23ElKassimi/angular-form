1000e94453c59add13596fe92fbccf194ced11ce
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-06-26 21:59:26 +0200

    - Creato FormGroup - Sincronizzazione con la oarte HTML

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormComponent implements OnInit {
 
+  homeform!: FormGroup;
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+    
+    this.homeform = new FormGroup({
+      // Define your form controls and validators here
+      name: new FormControl ('', Validators.required),
+      email: new FormControl('',[Validators.required, Validators.email]),
+      color: new FormControl(''), 
+      intext: new FormControl(''),
+       
+    })
+
+  }
+
+  onSubmit(): void{
+    console.log(this.homeform)
   }
 
 }
