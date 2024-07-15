2891a6c2aabf27af259324765b349fec84b33c49
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-07-15 20:32:50 +0200

    small bug fixed

--- src/app/service/firebase.service.ts
+++ src/app/service/firebase.service.ts
@@ -12,12 +12,12 @@ export class FirebaseService {
   url: string = 'https://corso-angular-5a5b9-default-rtdb.europe-west1.firebasedatabase.app/persone'
 
   insertPersona(body: {}) {
-    return this.http.post(this.url, body)
+    return this.http.post(this.url + ".json", body)
   }
 
 
   getPersone() {
-    return this.http.get(this.url)
+    return this.http.get(this.url + ".json")
   }
 
 
