4a08bf35878c0500d53aa0ec5af7911b210021b1
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-07-19 11:47:42 +0200

    CSS Update form Fixed

--- src/app/components/updateform/updateform.component.ts
+++ src/app/components/updateform/updateform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from '../..//service/firebase.service';
 @Component({
   selector: 'app-updateform',
   templateUrl: './updateform.component.html',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateformComponent implements OnInit {
 
-  constructor() { }
+
+  homeform!: FormGroup;
+
+
+  constructor(private fireBase: FirebaseService
+  ) { }
 
   ngOnInit(): void {
+
+    this.homeform = new FormGroup({
+      // Define your form controls and validators here
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      color: new FormControl('', Validators.required),
+      intext: new FormControl('', Validators.required),
+    })
+
+
   }
+
+  onSubmit(): void {
+    console.log(this.homeform)
+
+    this.fireBase.insertPersona(
+      {
+        name: this.homeform.value.name,
+        email: this.homeform.value.email,
+        color: this.homeform.value.color,
+        intext: this.homeform.value.intext
+      }
+    ).subscribe(data => { console.log(data) })
+  }
+
+
+
 
 }
