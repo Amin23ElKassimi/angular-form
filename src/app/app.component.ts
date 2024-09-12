37ec9be7f8948c779b5dc05b906c2603bacabed3
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-09-12 19:46:19 +0200

    ✅Rendere permanente il Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // Variables
   title = 'angular-form';
+
+
+  constructor(private authservice: AuthService) { }
+
+
+  ngOnInit(): void {
+
+    // Se lutente e' salvato nello storage ripolpami la variabile
+    if (localStorage.getItem('user')) {
+      const user = JSON.parse(localStorage.getItem("user"))
+      this.authservice.createUser(user.email, user.id, user._token, user._expirationDate)
+    }
+
+  }
+
+
+
+
 }
+
